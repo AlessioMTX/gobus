88fcd51457b2162583890deac3c0e5214e3f63aa
claudio <claudio.pomo@gmail.com>
2014-11-21 13:47:25 +0100

    first commit

--- mobile/src/main/java/com/dfba/gobus/MainActivity.java
+++ mobile/src/main/java/com/dfba/gobus/MainActivity.java
@@ -2,16 +2,42 @@ package com.dfba.gobus;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.location.*;
 
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getApplicationContext();
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                //makeUseOfNewLocation(location);
+                Log.e("position", Double.toString(location.getLatitude())+" "+Double.toString(location.getLongitude()));
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5, 500, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3, 0, locationListener);
+
+
     }
 
 
