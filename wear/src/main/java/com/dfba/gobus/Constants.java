6d0b9b1602183a325d7c16b0a324e9f1fc1bb324
claudio <claudio.pomo@gmail.com>
2014-11-21 19:57:02 +0100

    first commit

---
+++ wear/src/main/java/com/dfba/gobus/Constants.java
@@ -0,0 +1,15 @@
+package com.dfba.gobus;
+
+/**
+ * Created by claudio on 21/11/14.
+ */
+
+public class Constants {
+
+    public static final String NOTIFICATION_PATH = "/notification";
+    public static final String NOTIFICATION_TIMESTAMP = "timestamp";
+    public static final String NOTIFICATION_TITLE = "title";
+    public static final String NOTIFICATION_CONTENT = "content";
+
+    public static final String ACTION_DISMISS = "com.dfba.gobus.DISMISS";
+}
